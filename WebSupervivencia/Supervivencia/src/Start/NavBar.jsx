0d894150f4433cce9f3486a0ed75b2c1a8fad9d0
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-04-10 12:25:16 +0200

    Hamburguesa funciona
    queda darle el estilo deseado al display block cuando se pulsa la hamburguesa

--- WebSupervivencia/Supervivencia/src/Start/NavBar.jsx
+++ WebSupervivencia/Supervivencia/src/Start/NavBar.jsx
@@ -4,34 +4,45 @@ import hamburguesa from '../assets/Start/Logos/new_burger.png'
 
 import '../CSS/Start.css';
 import '../CSS/BootstrapEdit.css';
-import { useState ,useEffect} from 'react';
+import {useState ,useEffect} from 'react';
 import React from 'react';
 
 function navBar() {
  
+    //Control de como mostar el navList
+    // estado 0: oculta el nav | estado 1: muestra con display block | estado 2: muestra con display flex
     const [showMenu,setShowMenu] = useState(2)
+    //Control de que se haya pulsado la hamburguesa
+    const [manualMenuOpen, setManualMenuOpen] = useState(false);
+    //Control transiccion de ancho de pantalla a menor 750px
+    const [below750px, setBelow750px] = useState(window.innerWidth <= 750);
 
     const toggleMenu = () => {
+        setManualMenuOpen(prevState => !prevState);
         setShowMenu(prevState => (prevState + 1)%2);
     };
-
-    // 0 oculta 1 block 2 flex
 
     useEffect(() => {
         const handleResize = () => {
             const screenWidth = window.innerWidth;
-            if (screenWidth <= 700) {
-                setShowMenu(0);
-            } else {
+            const wasBelow750px = below750px;
+            const isBelow750px = screenWidth <= 750;
+
+            if (isBelow750px && !wasBelow750px && !manualMenuOpen) { // Oculta el menú al pasar por debajo de 750px y si no se ha pulsado la hamburguesa
+                setShowMenu(0); 
+            } else if(screenWidth > 750) { //Vuelve a mostrar el menu con flex y setea la hamburguesa a no pulsado
+                setManualMenuOpen(false);
                 setShowMenu(2);
             }
+
+            setBelow750px(isBelow750px);
         };
 
         window.addEventListener('resize', handleResize);
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, []);
+    }, [manualMenuOpen]);
 
     return(
         
@@ -44,8 +55,13 @@ function navBar() {
             
             <nav>
                 
-                <ul id="navList" style={{ display: showMenu === 0 ? 'none' : showMenu === 1 ? 'block' : 'flex' }}>
-
+                <ul id="navList" style={{ 
+                    display: showMenu === 0 ? 'none' : showMenu === 1 ? 'block' : 'flex',
+                    //Añadir el estilo deseado al display block
+                    // position: showMenu === 1 ? 'fixed' : '',
+                    // left: showMenu === 1 ? '0' : '',
+                    // paddingLeft: showMenu === 1 ? '75px' : '',
+                    }}>
                     <li>
                         <a>
                         <span className='animatedSpanNavBar'>Montaña</span>
