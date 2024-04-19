84e387830cdcb9a55443cd07735424fdd27d76d4
2tank <adrianfeve7@gmail.com>
2024-04-19 19:30:40 +0200

    Swipe girdCard implementado

--- WebSupervivencia/Supervivencia/src/GridZone/GridCard.jsx
+++ WebSupervivencia/Supervivencia/src/GridZone/GridCard.jsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import react, { useId } from 'react';
 
 import '../CSS/Grid.css';
 import {useState,useEffect,useRef} from 'react';
@@ -8,39 +8,74 @@ import infoCard from '../assets/Start/Logos/info.png'
 function GridCard({nameClass,nameVideo,texto}) {
 
     const[time2Play, setTime2Play] = useState(false)
+    const[reverseCard, setReverseCard] = useState(false)
 
-    const elementRef = useRef(null);
+
+    const videoId = useId();
+    const frontCardId = useId()
+    const reverseCardId = useId()
+
+    useEffect(() => {
+
+        const Fcard = document.getElementById(frontCardId);
+        const Bcard = document.getElementById(reverseCardId);
+        if(reverseCard){
+            Fcard.style.transform = 'perspective(600px) rotateY(180deg)';
+            Bcard.style.transform = 'perspective(600px) rotateY(360deg)';
+        }
+            
+    
+    },[reverseCard])
+
 
     const url = 'src/assets/VideosGrid/' + nameVideo + '.webm'
    
     useEffect(() => {
         
+        const videoCard = document.getElementById(videoId);
+
         if(time2Play){
-            elementRef.current.play()
+            videoCard.play()
         }else{
-            elementRef.current.pause()
+            videoCard.pause()
         }
 
     },[time2Play])
 
+
     return(
     <>
-    <div  onMouseEnter={() => {setTime2Play(true)} } onMouseLeave={() => {setTime2Play(false);}} id={nameClass} className="gridCard">
+
+    <div id={nameClass} className="gridCard">
+
+    <div id={frontCardId} className="frontCard" onMouseEnter={() => {setTime2Play(true)} } onMouseLeave={() => {setTime2Play(false);}}  >
         
-            <video ref={elementRef} control={"false"} src={url} muted></video>  
+        <video id={videoId} control={"false"} src={url} muted></video>  
         
         <Link to={'/' + nameVideo}>
-        <div className="gridDivContent">
+        <div className="flexDivContent">
                     <h3 className="gridText">
                         {texto}
                     </h3>
-                    <button>
-                        <img src={infoCard}></img>    
-                    </button> 
         </div> 
         </Link>
-       
+        <button onClick={() => {setReverseCard(true)}}>
+                        <img src={infoCard}></img>    
+        
+        </button> 
     </div>
+
+
+    <article id={reverseCardId} className="reverseCard">
+
+    <h1>resumen</h1>
+
+    </article>
+
+
+    </div>
+
+    
     </> 
     )
 }
