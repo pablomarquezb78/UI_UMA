d8cc8c854102db33643df1f7be72a8d45e7c96c9
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-04-20 21:52:59 +0200

    Help Sroll primeros pasos

--- WebSupervivencia/Supervivencia/src/GridZone/GridInfo.jsx
+++ WebSupervivencia/Supervivencia/src/GridZone/GridInfo.jsx
@@ -1,35 +1,57 @@
 import react from 'react';
 
 import '../CSS/Grid.css';
+import Accordion from 'react-bootstrap/Accordion';
 
 function GridInfo() {
 
    return(
-    
+    <>
     <section id="sectionGrid" className="vw-100 vh-100">
 
     <div className='gridInfoSection'>
-       
+    
         <div id="leftCard" className="gridCard">
             <h1>FAQ</h1>
-            <h5>¿Sobrevivir en ciudad?</h5>
-            <p>
-            Hemos planteado la supervivencia para un supuesto caso apocaliptico.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-            </p>
         </div>
 
-        <div id="rigthCard" className="gridCard">
+        <div id="rightCard" className="gridCard">
             <h1>Sobre Nosotros</h1>
-            <h2>Nos encantan las tetas gordas.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <p>Nos encantan las tetas gordas.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-            </h2>
+            </p>
         </div>
 
     </div>
 
     </section>
-
+    <Accordion defaultActiveKey="0" className='accordion'>
+    <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+    </Accordion.Item>
+    <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+    </Accordion.Item>
+    </Accordion>
+    </>
     )
 }
 
