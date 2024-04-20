36247a4738dd119d0e0dd4add9c6022d697084af
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-04-20 22:06:44 +0200

    Previo a cambio de formato de acordeon

--- WebSupervivencia/Supervivencia/src/GridZone/GridInfo.jsx
+++ WebSupervivencia/Supervivencia/src/GridZone/GridInfo.jsx
@@ -1,7 +1,6 @@
 import react from 'react';
 
 import '../CSS/Grid.css';
-import Accordion from 'react-bootstrap/Accordion';
 
 function GridInfo() {
 
@@ -17,7 +16,7 @@ function GridInfo() {
 
         <div id="rightCard" className="gridCard">
             <h1>Sobre Nosotros</h1>
-            <p>Nos encantan las tetas gordas.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             </p>
         </div>
@@ -25,32 +24,7 @@ function GridInfo() {
     </div>
 
     </section>
-    <Accordion defaultActiveKey="0" className='accordion'>
-    <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-    </Accordion.Item>
-    <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-    </Accordion.Item>
-    </Accordion>
+    
     </>
     )
 }
