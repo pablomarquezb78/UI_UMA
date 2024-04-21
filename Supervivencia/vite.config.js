964f5c56e576d5cac6b7e8ead7cee0c350428649
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-04-21 19:41:10 +0200

    Primera entrega del proyecto (Esta si)

--- Supervivencia/vite.config.js
+++ Supervivencia/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base: 'https://pablomarquezb78.github.io/UI_UMA',
+  base: '/vite-react-route/',
 })
