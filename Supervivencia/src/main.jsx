2c50d1da7c3f17553048deba79f2b59e76a08761
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-04-21 20:01:01 +0200

    nuevo intento

--- Supervivencia/src/main.jsx
+++ Supervivencia/src/main.jsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './CSS/main.css'
 import './CSS/Start.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import Index from './Index.jsx'
 
+const router = createBrowserRouter([
+  {
+    path:"/UI_UMA/",
+    element: <Index />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
