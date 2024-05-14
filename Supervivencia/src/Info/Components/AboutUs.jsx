575840e729dd551faa6b6cae65a2b5a9a7a7fbd9
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-05-14 22:37:36 +0200

    aboutUs responsive
    Se ha hecho todo lo posible en incluso lo imposible si es que lo imposible es posible

--- Supervivencia/src/Info/Components/AboutUs.jsx
+++ Supervivencia/src/Info/Components/AboutUs.jsx
@@ -5,19 +5,18 @@ import '../Info.css';
 function AboutUs() {
     return (
         <div id="container" className="container">
-            <div id="navSimulator" className="row"></div>
-            <div className="row justify-content-center align-items-center w-100 fontControl py-3 text-center">
+            <div id="titleContainer" className="row justify-content-center align-items-center w-100 fontControl text-center">
                 <h1>Sobre Nosotros</h1>
             </div>
-            <div className="row justify-content-center align-items-center w-100 fontControl py-3 text-center">
+            <div id="textContainer" className="row w-100 fontControl text-center">
                 <p>
                     Somos un grupo de estudiantes de tercer curso de Ingeniería del Software en la Universidad de Málaga,
                      este es nuestro proyecto para la asignatura "Interfaces de Usuario", donde hemos creado una página web
                      orientada a los tips de supervivencia a nivel recreativo de la forma más usable, accesible y atractiva
-                     posible. Esperemos que os guste :
+                     posible. Esperemos que os guste:
                 </p>
             </div>
-            <div id="imgContainer" className="row justify-content-center align-items-center h-50 w-100">
+            <div id="imgContainer" className="row justify-content-center align-items-center w-100">
                 <img src={APND} alt="APND Logo" className="img-fluid" />
             </div>
         </div>
