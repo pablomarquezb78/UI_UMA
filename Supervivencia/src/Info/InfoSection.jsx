194e5ac6a7833d1c9e44c5f7290a5c2899bac88d
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-05-15 11:39:08 +0200

    fontControl en InfoSection

--- Supervivencia/src/Info/InfoSection.jsx
+++ Supervivencia/src/Info/InfoSection.jsx
@@ -14,24 +14,24 @@ function GridInfo() {
     <div id="infoSection" className='bg-light d-flex align-items-start justify-content-around py-5 px-3'>
 
         <div className='px-1'>
-            <h5 className='pb-4'>Ayuda</h5>
-            <Link to="/UI_UMA/FAQ" className='text-decoration-none'>Preguntas Frecuentes</Link>
+            <h5 className='pb-4 fontControlInfoSectionh5'>Ayuda</h5>
+            <Link to="/UI_UMA/FAQ" className='text-decoration-none fontControlInfoSection'>Preguntas Frecuentes</Link>
         </div>
         <div className='px-1'>
-            <h5 className='pb-4'>Contacto</h5>
-            <a>Correo: APND@gmail.com</a><br/>
-            <a>Teléfono: 123-456-789</a><br/>
+            <h5 className='pb-4 fontControlInfoSectionh5'>Contacto</h5>
+            <a className='fontControlInfoSection text-decoration-none'>Correo: APND@gmail.com</a><br/>
+            <a className='fontControlInfoSection text-decoration-none'>Teléfono: 123-456-789</a><br/>
             <div id='socialNetWorkDisplayer' className='d-flex justify-content-around'>
                 <InstagramIcon/>
                 <XIcon/>
             </div>
         </div>
         <div className='px-1'>
-            <h5 className='pb-4'>Información</h5>
-            <Link to="/UI_UMA/AboutUs" className='text-decoration-none'>Sobre Nosotros</Link><br/>
-            <a>Política de Privacidad</a><br/>
-            <a>Aviso Legal</a><br/>
-            <a>Política de Cookies</a>
+            <h5 className='pb-4 fontControlInfoSectionh5'>Información</h5>
+            <Link to="/UI_UMA/AboutUs" className='text-decoration-none fontControlInfoSection'>Sobre Nosotros</Link><br/>
+            <a className='fontControlInfoSection text-decoration-none'>Política de Privacidad</a><br/>
+            <a className='fontControlInfoSection text-decoration-none'>Aviso Legal</a><br/>
+            <a className='fontControlInfoSection text-decoration-none'>Política de Cookies</a>
         </div>
 
     </div>
