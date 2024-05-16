cb343aea9facd68787eae441483600341e579cc7
pabaguilar <124668660+pabaguilar@users.noreply.github.com>
2024-05-17 00:21:01 +0200

    Juego de costa implementaciones terminadas

--- Supervivencia/src/Coast/HigherOrLower.jsx
+++ Supervivencia/src/Coast/HigherOrLower.jsx
@@ -1,18 +1,22 @@
-// HigherLowerGame.jsx
-
 import React, { useState, useEffect } from 'react';
 import data from './RandomImage.jsx';
 import './Coast.css';
-import { Translate } from '@mui/icons-material';
 
 function HigherLowerGame() {
+
     const [leftCardIndex, setLeftCardIndex] = useState(0);
     const [rightCardIndex, setRightCardIndex] = useState(1);
     const [imageArray, setImageArray] = useState([]);
-    const [isCorrect, setIsCorrect] = useState(0);
-    const [showTickAnimation, setShowTickAnimation] = useState(false);
-    const [showCrossAnimation, setShowCrossAnimation] = useState(false);
     const [showDeaths, setShowDeaths] = useState(false);
+    const [buttonsVisible, setButtonsVisible] = useState(true);
+
+    const [isCorrect, setIsCorrect] = useState(0);
+    const [tickAnimation, setTickAnimation] = useState(false);
+    const [crossAnimation, setCrossAnimation] = useState(false);
+
+    const [showAnimation, setShowAnimation] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const [showResult, setShowResult] = useState(false); 
 
     useEffect(() => {
         const numberOfCardAux = new Array(data.longData()).fill().map((_, index) => index + 1);
@@ -20,39 +24,54 @@ function HigherLowerGame() {
         setImageArray(shuffleCard);
     }, []);
 
-    useEffect(() => {
-        if (rightCardIndex === imageArray.length) {
-            window.alert('GANASTE');
-            setIsCorrect(0);
-            resetGame();
-        }
-    }, [rightCardIndex]);
 
     useEffect(() => {
-        if (showTickAnimation) {
+        setShowAnimation(true);
+        setTimeout(() => {
+            setShowAnimation(false);
+        }, 500);
+    }, [rightCardIndex, showResult]);
+
+
+    useEffect(() => {
+        if (tickAnimation) {
             const timer = setTimeout(() => {
-                setLeftCardIndex(rightCardIndex);
-                setRightCardIndex(rightCardIndex + 1);
-                setIsCorrect(0);
-                setShowTickAnimation(false);
-                setShowDeaths(false);
+                if(rightCardIndex + 1 > imageArray.length - 1){
+                    setShowResult(true);
+                    setTickAnimation(false);
+                }else{
+                    setLeftCardIndex(rightCardIndex);
+                    setRightCardIndex(rightCardIndex + 1);
+                    setIsCorrect(0);
+                    setTickAnimation(false);
+                    setShowDeaths(false);
+                    setButtonsVisible(true);
+                }
+
             }, 1500);
             return () => clearTimeout(timer);
         }
-    }, [showTickAnimation]);
+    }, [tickAnimation]);
 
     const handleHigherClick = () => {
+        setButtonsVisible(false);
         if (parseInt(data.randomImage(imageArray[leftCardIndex]).mortalidad) < parseInt(data.randomImage(imageArray[rightCardIndex]).mortalidad)) {
             setIsCorrect(1);
             setShowDeaths(true);
-            setShowTickAnimation(true);
+            setButtonsVisible(false);
+            setTickAnimation(true);
+            setTimeout(() => {
+                setCounter(counter+1);
+            }, 1500);
+            
         } else {
             setIsCorrect(2);
             setShowDeaths(true);
-            setShowCrossAnimation(true);
+            setCrossAnimation(true);
+            setButtonsVisible(false);
             setTimeout(() => {
-                resetGame();
-                setShowCrossAnimation(false);
+                setCrossAnimation(false);
+                setShowResult(true); 
             }, 1500); 
         }
     };
@@ -61,26 +80,40 @@ function HigherLowerGame() {
         if (parseInt(data.randomImage(imageArray[leftCardIndex]).mortalidad) > parseInt(data.randomImage(imageArray[rightCardIndex]).mortalidad)) {
             setIsCorrect(1);
             setShowDeaths(true);
-            setShowTickAnimation(true);
+            setTickAnimation(true);
+            setTimeout(() => {
+                setCounter(counter+1);
+            }, 1500);
+            setButtonsVisible(false);
         } else {
             setIsCorrect(2);
             setShowDeaths(true);
-            setShowCrossAnimation(true);
+            setCrossAnimation(true);
+            setButtonsVisible(false);
+            
             setTimeout(() => {
-                resetGame();
-                setShowCrossAnimation(false);
+                setCrossAnimation(false);
+                setShowResult(true);
             }, 1500); 
         }
     };
 
     const resetGame = () => {
-        setLeftCardIndex(0);
-        setRightCardIndex(1);
         const numberOfCardAux = new Array(data.longData()).fill().map((_, index) => index + 1);
         const shuffleCard = numberOfCardAux.sort(() => Math.random() - 0.5);
         setImageArray(shuffleCard);
+        setLeftCardIndex(0);
+        setRightCardIndex(1);
         setIsCorrect(0);
         setShowDeaths(false);
+        setCounter(0);
+        setButtonsVisible(true);
+        setShowResult(false); 
+        setReset(false);
+    };
+
+    const cancelGame = () => {
+        setShowResult(false);
     };
 
     return (
@@ -102,23 +135,39 @@ function HigherLowerGame() {
                         <h3 className='card-text'> {data.randomImage(imageArray[rightCardIndex])?.nombre}: {showDeaths ? data.randomImage(imageArray[rightCardIndex])?.mortalidad : '?'} muertes al año </h3>
                     </div>
                     <div className='botonesHOL position-absolute'>
-                        <button className='custom-button mb-1' onClick={handleHigherClick}>Higher</button>
-                        <button className='custom-button' onClick={handleLowerClick}>Lower</button>
+                        {buttonsVisible && (
+                            <>
+                                <button className='custom-button mb-1' onClick={handleHigherClick}>Higher</button>
+                                <button className='custom-button' onClick={handleLowerClick}>Lower</button>
+                            </>
+                        )}
                     </div> 
                 </div>
+                {showResult && (
+                    <div className=" puntuacionHOL position-absolute z-1 bg-white rounded">
+                        <div>
+                            <h2 className={`${isCorrect == 2 ? 'text-danger' : 'text-success'}`}>{isCorrect === 2 ? '¡Fallaste!' : '¡Ganaste!'}</h2>
+                            <button className='custom-button m-2' onClick={cancelGame}>Cancelar</button>
+                            <button className='custom-button m-2' onClick={resetGame}>Volver a jugar</button>      
+                        </div>
+                    </div>
+                )}
                 <div className={`position-absolute ${isCorrect !== 0 && 'fade-icon'}`}>
                     {isCorrect === 0 && <img src="src/assets/Coast/versus-icon.png"  alt="VS" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'black', borderRadius:'50%'}} />}
-                    {showTickAnimation && <img src="src/assets/Coast/tick-icon.png" alt="Tick" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'green', borderRadius:'50%' }} />}
+                    {isCorrect === 1 && <img src="src/assets/Coast/tick-icon.png" alt="Tick" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'green', borderRadius:'50%' }} />}
                     {isCorrect === 2 && <img src="src/assets/Coast/cross-icon.png" alt="Cross" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'red', borderRadius:'50%' }} />}
                 </div>
             </section>
-            <div>
+            <div className="d-flex justify-content-center align-items-center m-3 vw-100">
+                <button className='custom-button m-4' onClick={resetGame}>Reiniciar</button>
+                <h3 className={`${showAnimation ? "puntuacionHOL" : ""}`}>Puntuación: {counter}</h3>
             </div>
         </section>
     );
 }
 
 export default HigherLowerGame;
+
 
 
 
