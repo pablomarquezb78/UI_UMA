6a658dd569897183635d773836dfeba666447ebe
2tank <adrianfeve7@gmail.com>
2024-05-31 12:15:46 +0200

    TINDER VERSION MUY FINAL

--- Supervivencia/src/Selva/TinderCard.jsx
+++ Supervivencia/src/Selva/TinderCard.jsx
@@ -1,15 +1,15 @@
 import veneno from './FotosAlimentos/veneno.png';
 import comestible from './FotosAlimentos/saludable.png';
 
-function TinderCard({card, index, tab}) {
+function TinderCard({card, index}) {
     const urlImage = 'src/Selva/FotosAlimentos/' + card.foto;
     const nombre = card.nombre;
 
     return (
         <>
-            <img draggable="false" className='fruta' alt={index === 1 ? nombre : ''} src={urlImage} tabIndex={tab}></img>
+            <img draggable="false" className='fruta' alt={index === 1 ? nombre : ''} src={urlImage}></img>
             <img draggable="false" src={veneno} className='venenoso'></img>
-            <img draggable="false" src={comestible} className='comestible'></img>
+            <img draggable="false"  src={comestible} className='comestible'></img>
             <h2>{nombre}</h2>
         </>
     );
