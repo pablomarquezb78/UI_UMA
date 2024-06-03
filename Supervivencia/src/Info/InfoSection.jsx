62e6567559da82dd195161ca66e846c0c2707732
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-06-03 20:38:50 +0200

    shortCut chingon

--- Supervivencia/src/Info/InfoSection.jsx
+++ Supervivencia/src/Info/InfoSection.jsx
@@ -14,11 +14,11 @@ function InfoSection() {
     <div id="infoSection" className='position-relative bg-light d-flex align-items-start justify-content-around py-5 px-3'>
 
         <div className='px-1'>
-            <h1 className='pb-4 fontControlInfoSectionh4' tabIndex={0}>Ayuda</h1>
+            <h1 className='pb-4 fontControlInfoSectionh4'>Ayuda</h1>
             <Link to="/FAQ" className='text-decoration-none fontControlInfoSection'>Preguntas Frecuentes</Link>
         </div>
         <div className='px-1'>
-            <h1 className='pb-4 fontControlInfoSectionh4' tabIndex={0}>Contacto</h1>
+            <h1 className='pb-4 fontControlInfoSectionh4'>Contacto</h1>
             <a className='fontControlInfoSection text-decoration-none' tabIndex={0}>Correo: APND@gmail.com</a><br/>
             <a className='fontControlInfoSection text-decoration-none' tabIndex={0}>Teléfono: 123-456-789</a><br/>
             <div id='socialNetWorkDisplayer' className='d-flex justify-content-around'>
@@ -27,7 +27,7 @@ function InfoSection() {
             </div>
         </div>
         <div className='px-1'>
-            <h1 className='pb-4 fontControlInfoSectionh4' tabIndex={0}>Información</h1>
+            <h1 className='pb-4 fontControlInfoSectionh4'>Información</h1>
             <Link to="/AboutUs" className='text-decoration-none fontControlInfoSection'>Sobre Nosotros</Link><br/>
             <Link to="/UnderConstruction" className='fontControlInfoSection text-decoration-none'>Política de Privacidad</Link><br/>
             <Link to="/UnderConstruction" className='fontControlInfoSection text-decoration-none'>Aviso Legal</Link><br/>
