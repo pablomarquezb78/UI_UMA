d865a5bd9b97923b149c063d9eed003daf7d12f9
pabaguilar <124668660+pabaguilar@users.noreply.github.com>
2024-06-04 02:21:29 +0200

    Varios cambios al jogo de costa
    Me falta implementar la seccion de los shortcuts

--- Supervivencia/src/Coast/HelpHOL.jsx
+++ Supervivencia/src/Coast/HelpHOL.jsx
@@ -11,13 +11,13 @@ function HelpHOL({cancelGame}){
     return(
         <div className="position-absolute z-1 bg-white rounded p-1 border border-black w-75 d-flex flex-column justify-content-center align-items-center top-50 start-50 translate-middle" style={{maxWidth: '600px', height: '90%'}}>
             <span tabIndex='0' ref={ref1} style={{ fontSize: fontSize1, height: '10%', width: '100%', letterSpacing: '1px', lineHeight: '1.2', fontWeight: 'bold'}}>
-                Ayuda juego   
+                Higher or Lower   
             </span>
             <span tabIndex='0' ref={ref1} style={{ fontSize: fontSize1, height: '60%', width: '100%', letterSpacing: '1px', lineHeight: '1.2'}}>
                 El juego está basado en el típico juego de Higher Or Lower pero con una pequeña modificación.<br /><br />
-                Tienes que seleccionar si el ser vivo de la imagen de la derecha mata más o menos personas al año que el ser vivo de la izquierda.
+                Tienes que seleccionar si el ser vivo de la imagen de la derecha (o abajo) mata más o menos personas al año que el ser vivo de la izquierda.
             </span>
-            <button tabIndex='0' className='custom-button m-2' onClick={cancelGame}>Volver al juego</button>         
+            <button tabIndex='0' className='custom-button m-2' onClick={cancelGame}>Cancelar</button>         
         </div>
         // border: "1px solid red"
     );
--- Supervivencia/src/Coast/HigherOrLower.jsx
+++ Supervivencia/src/Coast/HigherOrLower.jsx
@@ -3,6 +3,7 @@ import data from './RandomImage.jsx';
 import { IconButton } from '@mui/material';
 import { styled } from '@mui/system';
 import SosIcon from '@mui/icons-material/Sos';
+import KeyboardIcon from '@mui/icons-material/Keyboard';
 import './Coast.css';
 import HelpHOL from './HelpHOL.jsx';
 
@@ -134,15 +135,15 @@ function HigherLowerGame() {
 
     useEffect(() => {
         const handleKeyDown = (e) => {
-            if (e.key.toLowerCase() === 'r') {
+            if (e.key.toLowerCase() === '0') {
                 resetGame();
-            } else if (e.key.toLowerCase() === 'h') {
+            } else if (e.key.toLowerCase() === '9') {
                 helpHandler();
             } else if (e.key === '8') {
                 handleHigherClick();
             } else if (e.key === '2') {
                 handleLowerClick();
-            } else if (e.key.toLowerCase() === 'c') {
+            } else if (e.key.toLowerCase() === '1') {
                 cancelGame();
             }
         };
@@ -162,7 +163,8 @@ function HigherLowerGame() {
                 <h2 className='tituloHOL'>Higher or Lower</h2>
                 <h2 className='tituloHOL'>¿Cuál es más letal para el humano 💀?</h2>
             </div>
-            <AnimatedIconButton className='helpHOL position-absolute' aria-label='Ayuda juego' aria-hidden='false' style={{ right: '0', top: '70%', transform: 'translateY(-50%)', marginRight: '1%' }} onClick={() => { helpHandler() }}><SosIcon/></AnimatedIconButton>
+            <AnimatedIconButton title="Atajos de teclado" aria-label='Atajos del juego' aria-hidden='false' className='position-absolute' id='keyboardHOL' style={{ right: '0', top: '70%', transform: 'translateY(-50%)', marginRight: '1%' }}><KeyboardIcon /></AnimatedIconButton>
+            <AnimatedIconButton title="Ayuda para el juego" className='sosHOL position-absolute' aria-label='Ayuda para el juego' aria-hidden='false' id='sosHOL' style={{ left: '0', top: '70%', transform: 'translateY(-50%)', marginLeft: '1%'}} onClick={() => { helpHandler() }}><SosIcon/></AnimatedIconButton>
         </div>
             <section className='imagenesHOL position-relative d-flex justify-content-center align-items-center'>
                 {needHelp && (<HelpHOL cancelGame={cancelGame}/>)}
@@ -174,14 +176,14 @@ function HigherLowerGame() {
                 </div>
                 <div className='imagenHOL position-relative'>
                     <img alt={data.randomImage(imageArray[rightCardIndex])?.imgAlt} className='img-fluid containerHOL' src={data.randomImage(imageArray[rightCardIndex])?.img}/>
-                    <div id='informacion_imagen_d'  className='position-absolute text-center z-0'>
+                    <div id='informacion_imagen_d'  className='position-absolute text-center'>
                         <h3 tabIndex='0' className='card-text'> {data.randomImage(imageArray[rightCardIndex])?.cardTitle}: {showDeaths ? data.randomImage(imageArray[rightCardIndex])?.mortalidad : '?'} muertes al año </h3>
                     </div>
                     <div className='botonesHOL position-absolute'>
                         {buttonsVisible && (
                             <>
-                                <button className='custom-button mb-1' onClick={handleHigherClick}>Higher</button>
-                                <button className='custom-button' onClick={handleLowerClick}>Lower</button>
+                                <button className='custom-buttonHOL mb-1' onClick={handleHigherClick}>Higher</button>
+                                <button className='custom-buttonHOL' onClick={handleLowerClick}>Lower</button>
                             </>
                         )}
                     </div> 
@@ -189,18 +191,18 @@ function HigherLowerGame() {
                 {showResult && (
                     <div className="position-absolute z-1 bg-white rounded p-2 border border-black" >
                         <h2 tabIndex='0' style={{fontSize:'calc(20px + (30 - 20) * ((100vmin - 350px) / (1080 - 350)))'}} className={`${isCorrect == 2 ? 'text-danger' : 'text-success'}`}>{isCorrect === 2 ? '¡Fallaste!' : '¡Ganaste!'}</h2>
-                        <button tabIndex='0' className='custom-button m-2' onClick={cancelGame}>Cancelar</button>
-                        <button tabIndex='0' className='custom-button m-2' onClick={resetGame}>Volver a jugar</button>      
+                        <button tabIndex='0' className='custom-buttonHOL m-2' onClick={cancelGame}>Cancelar</button>
+                        <button tabIndex='0' className='custom-buttonHOL m-2' onClick={resetGame}>Volver a jugar</button>      
                     </div>
                 )}
                 <div className={`position-absolute ${isCorrect !== 0 && 'fade-icon'}`}>
-                    {isCorrect === 0 && <img src="/Coast/CoastGame/versus-icon.png"  alt="VS" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'black', borderRadius:'50%'}} />}
-                    {isCorrect === 1 && <img src="/Coast/CoastGame/tick-icon.png" alt="Tick" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'green', borderRadius:'50%' }} />}
-                    {isCorrect === 2 && <img src="/Coast/CoastGame/cross-icon.png" alt="Cross" style={{ maxWidth: 'calc(50px + (100 - 50) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'red', borderRadius:'50%' }} />}
+                    {isCorrect === 0 && <img src="/Coast/CoastGame/versus-icon.png"  alt="VS" style={{ maxHeight: 'calc(40px + (100 - 40) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'black', borderRadius:'50%'}} />}
+                    {isCorrect === 1 && <img src="/Coast/CoastGame/tick-icon.png" alt="Tick" style={{ maxHeight: 'calc(40px + (100 - 40) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'green', borderRadius:'50%' }} />}
+                    {isCorrect === 2 && <img src="/Coast/CoastGame/cross-icon.png" alt="Cross" style={{ maxHeight: 'calc(40px + (100 - 40) * ((100vmin - 350px) / (1080 - 350)))', backgroundColor: 'red', borderRadius:'50%' }} />}
                 </div>
             </section>
             <div className="d-flex justify-content-center align-items-center">
-                <button className={`custom-button m-3 ${!showResult && !crossAnimation && !tickAnimation ? '' : 'esconderBoton'}`} onClick={resetGame}>Reiniciar</button>
+                <button className={`custom-buttonHOL m-3 ${!showResult && !crossAnimation && !tickAnimation ? '' : 'esconderBoton'}`} onClick={resetGame}>Reiniciar</button>
                 <h3 tabIndex='0' style={{fontSize:'calc(15px + (30 - 15) * ((100vmin - 350px) / (1080 - 350)))'}} className={`${showAnimation ? "puntuacionHOL" : ""}`}>Puntuación: {counter}</h3>
             </div>
         </section>
