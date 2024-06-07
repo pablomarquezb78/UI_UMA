b95294b085717a29e71f61267b3b19d6d95cafa7
pabaguilar <124668660+pabaguilar@users.noreply.github.com>
2024-06-07 13:48:14 +0200

    marcha atrás (juanmi)

--- Supervivencia/src/Coast/CoastMain.jsx
+++ Supervivencia/src/Coast/CoastMain.jsx
@@ -30,7 +30,7 @@ function CoastMain(){
 
                 <div className='sliderItem sliderTips'>
                     <article className='tipsSupervivencia'>
-                        <h1 aria-label='Tips de supervivencia en la Costa'>Tips de supervivencia en la Costa:</h1>
+                        <h1>Tips de supervivencia en la Costa:</h1>
                         <h2>Prepárate adecuadamente🎒:</h2>
                         <p>
                             Lleva equipo esencial como un botiquín de primeros auxilios, un cuchillo, linterna, mapas, brújula o GPS, protector solar, y ropa adecuada para el clima. 
